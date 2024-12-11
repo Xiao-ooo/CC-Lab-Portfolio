156ce1cd697749a90ccdbf740d012297fcdb18a2
Xiao-ooo <xiaoqingxing12@gmail.com>
2024-12-11 15:20:40 -0500

    Margin

--- Project 7/P7sketch.js
+++ Project 7/P7sketch.js
@@ -16,31 +16,25 @@ let myMusic;
 
 function preload() {
   
-//Fonts credit to: PressStart2P
-  // myFont = loadFont('PressStart2P-Regular.ttf');
-  myFont = loadFont('CC-Lab-Portfolio-main/Project 7/P7Font.ttf');
-
-//BG Music credit to: Dread Pitt "Fear"
-  // myMusic = loadSound('fear-dread-pitt-main-version-296.mp3');
-  myMusic = loadSound('CC-Lab-Portfolio-main/Project 7/P7 Sound.mp3');
+  //Fonts credit to: PressStart2P
+    myFont = loadFont('P7Font.ttf');
   
-//My MAIN Character image - Credit to myself
-  // img = loadImage('MyCharacter.png');
-  img = loadImage('CC-Lab-Portfolio-main/Project 7/P7MyCharacter.png');
+  //BG Music credit to: Dread Pitt "Fear"
+    myMusic = loadSound('P7 Sound.mp3');
+    
+  //My MAIN Character image - Credit to myself
+    img = loadImage('P7MyCharacter.png');
+    
+  //BedRoom Floor Tiles image credit to: Misfitoki
+    floor = loadImage ('P7BedRTile.jpg');
   
-//BedRoom Floor Tiles image credit to: Misfitoki
-  // floor = loadImage ('pixel_art_study___tiles__1_by_misfitoki_deqc368-fullview.jpg');
-  floor = loadImage ('CC-Lab-Portfolio-main/Project 7/P7BedRTile.jpg');
-
-//BathRoom Floor Tiles image - credit to myself
-  // bathroomTile = loadImage('bathroomTile.png');
-  bathroomTile = loadImage('CC-Lab-Portfolio-main/Project 7/P7BathRTile.png');
-
-//LivingRomm Floor Tiles image credit to: Diego Lopez  
-  // livingFloor = loadImage('diego-lopez-groundtiles.jpg');
-  livingFloor = loadImage('CC-Lab-Portfolio-main/Project 7/P7LivingRTile.jpg');
-
-}
+  //BathRoom Floor Tiles image - credit to myself
+    bathroomTile = loadImage('P7BathRTile.png');
+  
+  //LivingRomm Floor Tiles image credit to: Diego Lopez  
+    livingFloor = loadImage('P7LivingRTile.jpg');
+  
+  }
 
 function setup() {
   createCanvas(600, 400).parent("sketch-container");
